a44bc6fbc7fb89a53475b06ed62b543953cab37e
mykyta-boichuk <50881562+mykyta-boichuk@users.noreply.github.com>
2022-09-18 20:42:55 +0300

    UD-0001.  Обновление дизайна для NavBar
    add icons

--- client/src/components/common/AppNavBar/index.tsx
+++ client/src/components/common/AppNavBar/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import classes from "./styles.module.scss"
+import { ImUsers, ImUserTie } from "react-icons/im";
+import { BsLightbulbFill } from "react-icons/bs";
+import { IoTicket } from "react-icons/io5";
+import { IoIosJournal, IoIosSettings, IoMdMedal, IoIosPie } from "react-icons/io";
 
 export const AppNavBar = () => {
     return (
@@ -9,17 +13,17 @@ export const AppNavBar = () => {
             </div>
             <div className={classes.navList}>
                 <ul>
-                    <li>Overview</li>
-                    <li>Tickets</li>
-                    <li>Ideas</li>
-                    <li>Contacts</li>
-                    <li>Agents</li>
-                    <li>Articles</li>
+                    <li>< IoIosPie />Overview</li>
+                    <li>< IoTicket />Tickets</li>
+                    <li>< BsLightbulbFill />Ideas</li>
+                    <li>< ImUsers /> Contacts</li>
+                    <li>< ImUserTie /> Agents</li>
+                    <li>< IoIosJournal />Articles</li>
                 </ul>
             <div className={classes.lane} ></div>
                 <ul>
-                    <li>Settings</li>
-                    <li>Subscriptions</li>
+                    <li>< IoIosSettings />Settings</li>
+                    <li>< IoMdMedal />Subscriptions</li>
                 </ul>
             </div>
         </nav>
